e04c8b518bc7529000b497b17ab692acd7e6061e
Evzi Stara <84392323+evzistara@users.noreply.github.com>
2026-07-01 07:59:03 +0000

    Adjust width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,12 @@
 import { GiBookCover } from "react-icons/gi";
 
-export default function Header() { 
-
-return(
-   
-    <div className=" bg-primary text-white p-4 text-2xl"> 
-    <div className="flex items-center gap-4 max-w-6xl m-auto">
+export default function Header() {
+  return (
+    <div className=" bg-primary text-white p-4 text-2xl">
+      <div className="flex items-center gap-4 max-w-7xl m-auto">
         <GiBookCover />
         <p>Library</p>
+      </div>
     </div>
-    </div>
-
-)
+  );
 }
