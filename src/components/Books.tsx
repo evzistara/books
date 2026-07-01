ce5d910a1fb64698ab8dfd7f775586e8f16668f0
Evzi Stara <84392323+evzistara@users.noreply.github.com>
2026-07-01 08:00:04 +0000

    Add all visual parts

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,19 +1,67 @@
-export default function Books () {
-    return (
-        <>
-        <div className="bg-background h-dvh">
-        <div className="max-w-6xl m-auto">
-            <div className="flex justify-between pt-10 pb-10">
-                <div>
-                    <button className="px-4 py-2 bg-button-background">All</button>
-                    <button className="px-4 py-2 bg-button-background">Unread</button>
-                    <button className="px-4 py-2 bg-button-background">Read</button>
-                </div>
-                <button className="px-4 py-2 bg-CTA text-white font-semibold">+ Add book</button>
+import { useState } from "react";
+import { FaRegTrashCan } from "react-icons/fa6";
+import Popup from "./Popup";
+
+export default function Books() {
+
+  return (
+    <>
+      <div className="bg-background h-dvh p-5">
+        <div className="max-w-7xl m-auto">
+          <div className="flex justify-between pt-10 pb-10">
+            <div>
+              <button className="px-4 py-2 bg-button-background">All</button>
+              <button className="px-4 py-2 bg-button-background">Unread</button>
+              <button className="px-4 py-2 bg-button-background">Read</button>
+            </div>
+            <Popup />
+          </div>
+
+          <div className="md:grid grid-cols-2 gap-5 lg:grid-cols-3">
+            <div className="bg-box-background p-4 border border-t-5 border-button-background">
+              <div className="flex justify-between items-center">
+                <span className="bg-button-background px-2 py-1 text-xs">
+                  UNREAD
+                </span>
+                <button>
+                  <FaRegTrashCan />
+                </button>
+              </div>
+
+              <p className="mt-4 font-semibold text-text">Name of the book</p>
+              <p className="mt-1 mb-4">Author</p>
+              <hr className="text-gray-300" />
+              <div className="mt-4 flex justify-between items-center">
+                <p className="text-xs">Added date</p>
+                <button className="text-xs px-3 py-1 border border-primary text-primary font-semibold">
+                  ✓ Mark read
+                </button>
+              </div>
             </div>
 
+            <div className="bg-box-background p-4 border border-t-5 border-button-background border-t-primary">
+              <div className="flex justify-between items-center">
+                <span className="bg-primary-100 text-primary bg-primary-light px-2 py-1 text-xs">
+                  READ
+                </span>
+                <button>
+                  <FaRegTrashCan />
+                </button>
+              </div>
+
+              <p className="mt-4 font-semibold">Name of the book</p>
+              <p className="mt-1 mb-4">Author</p>
+              <hr className="text-gray-300" />
+              <div className="mt-4 flex justify-between items-center">
+                <p className="text-xs">Added date</p>
+                <button className="text-xs px-3 py-1 border border-button-foreground text-button-foreground font-semibold">
+                  Mark unread
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
